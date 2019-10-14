0b3ee1c75aab0f806c7ef84cadb6b20200fa928f
lukas <np-hardass@protonmail.com>
2019-10-14 16:21:43 +0200

    dont know what i changed

--- main.cpp
+++ main.cpp
@@ -20,10 +20,7 @@ public:
     }
     double elapsed() const
     {
-        return std::chrono::duration_cast<second_>(clock_::now() - beg_).count();
-    }
-
-private:
+        return std::chrono::duration_cast<second_>(clock_::now() - beg_).count();} private:
     typedef std::chrono::high_resolution_clock clock_;
     typedef std::chrono::duration<double, std::ratio<1>> second_;
     std::chrono::time_point<clock_> beg_;
@@ -64,8 +61,11 @@ auto main() -> int
 
         auto distance = pathfinder.shortestDistanceFromTo(from, to);
         auto normal_time = t.elapsed();
+        t.reset();
         auto distances = pathfinder.shortestDistanceFromTo(from, {to, ++to, ++to, ++to, ++to, ++to, ++to, ++to, ++to});
+        auto multi_time = t.elapsed();
         fmt::print("calculated distances in: {}s\n", normal_time);
+        fmt::print("calculated multi distances in: {}s\n", multi_time);
         fmt::print("ch_distance:\t{}\n", ch_distance);
         fmt::print("distance:\t{}\n", distance);
         fmt::print("distances:\t{}\n", distances);
